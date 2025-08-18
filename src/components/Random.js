c571fa3a49a1398ee092f5f1e2b210730742b098
ajay-kotambe <kotambeajay07@gmail.com>
2025-08-18 21:14:23 +0530

    added env file update random.js

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,8 +1,15 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./Random.css";
+import axios from "axios";
+
+const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
 const Random = () => {
   const [gif, setGif] = useState("");
+  async function fetchData() {
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+    const output = await axios.get(url);
+  }
 
   const clickHandler = () => {};
   return (
@@ -12,7 +19,9 @@ const Random = () => {
       </h2>
 
       <img src={gif} alt="" width={450} />
-      <button onClick={clickHandler}>Generate</button>
+      <button onClick={clickHandler} className="">
+        Generate
+      </button>
     </div>
   );
 };
