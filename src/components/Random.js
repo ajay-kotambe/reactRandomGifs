ebea44ca0578e7b7eb3b8a965e0be69951bd9cd7
ajay-kotambe <kotambeajay07@gmail.com>
2025-08-18 21:45:11 +0530

    random function ready, added loader, working on tag.js

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,24 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Random.css";
 import axios from "axios";
+import Loader from "./Loader";
 
 const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
 const Random = () => {
   const [gif, setGif] = useState("");
+  const [loading, setLoading] = useState(false);
   async function fetchData() {
+    setLoading(true);
     const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
-    const output = await axios.get(url);
+    const { data } = await axios.get(url);
+    const gifSource = data.data.images.downsized_large.url;
+    console.log(gifSource);
+    setGif(gifSource);
+    setLoading(false)
   }
 
-  const clickHandler = () => {};
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const clickHandler = () => {
+    fetchData();
+  };
   return (
     <div className="random-gif-container">
       <h2>
         <u> A Random GIFS</u>
       </h2>
+      {loading ? (
+        <Loader />
+      ) : (
+        <img src={gif} alt="RandomGIFS" width={450} height={290} />
+      )}
 
-      <img src={gif} alt="" width={450} />
       <button onClick={clickHandler} className="">
         Generate
       </button>
--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from "react";
+
+import { useEffect, useState } from "react";
+import "./Tag.css";
+import axios from "axios";
+import Loader from "./Loader";
+
+const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
 const Tag = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [gif, setGif] = useState("");
+  const [loading, setLoading] = useState(false);
+  async function fetchData() {
+    setLoading(true);
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+    const { data } = await axios.get(url);
+    const gifSource = data.data.images.downsized_large.url;
+    console.log(gifSource);
+    setGif(gifSource);
+    setLoading(false);
+  }
 
-export default Tag
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const clickHandler = () => {
+    fetchData();
+  };
+  return (
+    <div className="random-gif-container">
+      <h2>
+        <u> A Random GIFS</u>
+      </h2>
+      {loading ? (
+        <Loader />
+      ) : (
+        <img src={gif} alt="RandomGIFS" width={450} height={290} />
+      )}
+
+      <button onClick={clickHandler} className="">
+        Generate
+      </button>
+    </div>
+  );
+};
+
+export default Tag;
