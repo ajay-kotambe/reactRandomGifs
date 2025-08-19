a3e17833e6876c6fa62625e2d699f79bb0053d77
ajay-kotambe <kotambeajay07@gmail.com>
2025-08-19 19:30:11 +0530

    creating hooks

--- src/hooks/useGif.js
+++ src/hooks/useGif.js
@@ -4,12 +4,14 @@ import axios from "axios";
 import Loader from "./Loader";
 
 const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+const tagMemeUrl =
+  "https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}";
+const randomMemeUrl = "https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}";
 
-const Tag = () => {
+const Tag = ({ tag }) => {
   const [gif, setGif] = useState("");
-  const [tag, setTag] = useState("");
-
   const [loading, setLoading] = useState(false);
+
   async function fetchData() {
     setLoading(true);
     const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
@@ -19,33 +21,12 @@ const Tag = () => {
     setGif(gifSource);
     setLoading(false);
   }
-  const changeHandler = (event) => {
-    setTag(event.target.value);
-  };
 
   useEffect(() => {
     fetchData();
   }, []);
-  const clickHandler = () => {
-    fetchData();
-  };
-  return (
-    <div className="tag-gif-container">
-      <h2>
-        <u> A Random {tag} GIFS</u>
-      </h2>
-      {loading ? (
-        <Loader />
-      ) : (
-        <img src={gif} alt="RandomGIFS" width={450} height={290} />
-      )}
 
-      <input type="text" onChange={changeHandler} value={tag} />
-      <button onClick={clickHandler} className="">
-        Generate
-      </button>
-    </div>
-  );
+  return { gif, loading, fetchData };
 };
 
 export default Tag;
